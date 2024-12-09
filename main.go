cde92732f6553cdb7413756cae77608cbe69da1d
Rafael de Souza Silva <77814194+RafaSS@users.noreply.github.com>
2024-12-09 13:41:28 -0300

    Add logging middleware

--- main.go
+++ main.go
@@ -3,21 +3,29 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"time"
 )
 
+func Log(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		begin := time.Now()
+		next.ServeHTTP(w, r)
+		fmt.Println(r.URL.String(), r.Method, time.Since(begin))
+	})
+}
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc(
-		"/healthcheck",
+		"/api/users/{foo}",
 		func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintln(w, "hello, world")
 		},
 	)
 	srv := &http.Server{
 		Addr:                         ":8080",
-		Handler:                      mux,
+		Handler:                      Log(mux),
 		DisableGeneralOptionsHandler: false,
 		ReadTimeout:                  10 * time.Second,
 		WriteTimeout:                 10 * time.Second,
@@ -28,4 +36,29 @@ func main() {
 			panic(err)
 		}
 	}
+}
+func fetchPokemon() {
+	resp, err := http.Get("https://pokeapi.co/api/v2/pokemon/ditto")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error: status code", resp.StatusCode)
+		return
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return
+	}
+
+	fmt.Println(string(body))
+}
+
+func init() {
+	go fetchPokemon()
 }
