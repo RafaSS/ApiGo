5e0582c5262d6d89230ecaa72385e031b76276bf
Rafael de Souza Silva <77814194+RafaSS@users.noreply.github.com>
2024-12-10 17:58:55 -0300

    Update build configuration and restructure main application logic

---
+++ cmd/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"html/template"
+	"io"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+type Counter struct {
+	Count int
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func NewTemplates() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
+func main() {
+
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	e.Renderer = NewTemplates()
+	count := Counter{Count: 0}
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index", count)
+	})
+	e.POST("/count", func(c echo.Context) error {
+		count.Count++
+		return c.Render(200, "count", count)
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
